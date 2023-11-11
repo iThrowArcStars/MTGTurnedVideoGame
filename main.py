a24e54aae73afd5cdee6d83513644f170abd98bb
iThrowArcStars <austincfithian@gmail.com>
2023-11-10 20:26:35 -0800

    Combined the draw_seven.py file and main.py

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import textwrap
+import random
 from all_cards import *
 
+def print_card_info(card):
+    print(f"Name: {card.name}")
+    print(f"Type: {card.card_type}")
+
+    if hasattr(card, "abilities") and card.abilities:
+        print("Abilities:")
+        for ability in card.abilities:
+            wrapped_text = textwrap.fill(ability, width=80)  # Adjust the line width as needed
+            print(f"  - {wrapped_text}")
+
+    if hasattr(card, "mana_cost") and card.mana_cost:
+        print(f"Mana Cost: {', '.join(card.mana_cost)}")
+
+    print()
+
+def draw_seven_cards():
+    shuffled_deck = random.sample(all_cards, len(all_cards))
+    hand = shuffled_deck[:7]
+    return hand
+
 if __name__ == "__main__":
-    def print_card_info(card):
-        print(f"Name: {card.name}")
-        print(f"Type: {card.card_type}")
+    # Example: Print information for each drawn card
+    drawn_cards = draw_seven_cards()
 
-        if hasattr(card, "abilities") and card.abilities:
-            print("Abilities:")
-            for ability in card.abilities:
-                wrapped_text = textwrap.fill(ability, width=80)  # Adjust the line width as needed
-                print(f"  - {wrapped_text}")
-
-        if hasattr(card, "mana_cost") and card.mana_cost:
-            print(f"Mana Cost: {', '.join(card.mana_cost)}")
-
-        print()
-
-# Print card information
-print_card_info(atarka_world_render)
-print_card_info(archwing_dragon)
-print_card_info(lightning_bolt)
-print_card_info(street_spasm)
-print_card_info(curse_of_bloodletting)
+    print("Drawn Cards:")
+    for card in drawn_cards:
+        print_card_info(card)
