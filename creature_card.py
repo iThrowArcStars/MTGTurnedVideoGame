4274ad2f6be4670fb64256c509ade88b421ebbf0
iThrowArcStars <austincfithian@gmail.com>
2023-11-14 20:14:24 -0800

    Updated the CreatureCard readability

--- creature_card.py
+++ creature_card.py
@@ -25,24 +25,24 @@ atarka_world_render = CreatureCard(
     name="Atarka, World Render",
     power=6,
     toughness=4,
-    abilities=["Flying, trample \nWhenever a Dragon you control attacks,"
-            "it gains double strike until end of turn. "],
+    abilities=["Flying, trample "
+                "\nWhenever a Dragon you control attacks, it gains double strike until end of turn. "],
 )
 archwing_dragon = CreatureCard(
     creature_type=DRAGON,
     name="Archwing Dragon",
     power=4,
     toughness=4,
-    abilities=["Flying, haste \nAt the beggining of the end step, return Archwing"
-            " Dragon to its owner's hand."],
+    abilities=["Flying, haste "
+               "\nAt the beggining of the end step, return Archwing Dragon to its owner's hand."],
 )
 balefire_dragon = CreatureCard(
     creature_type=DRAGON,
     name="Balefire Dragon",
     power=6,
     toughness=6,
-    abilities=["Flying\nWhenever Balefire Dragon daels combat damage to a player"
-            ", it deals that much damage to each creature that player controls. "],
+    abilities=["Flying"
+               "\nWhenever Balefire Dragon daels combat damage to a player, it deals that much damage to each creature that player controls. "],
 )
 child_of_the_pack = CreatureCard(
     creature_type=HUMAN,
@@ -57,8 +57,8 @@ dragon_whelp = CreatureCard(
     name="Dragon Whelp",
     power=2,
     toughness=3,
-    abilities=["Flying\nChaosX1: +1/0; if more than three mana was spent this"
-    "way, Dragon Whelp is destroyed at the end of turn."],
+    abilities=["Flying"
+               "\nChaosX1: +1/0; if more than three mana was spent this way, Dragon Whelp is destroyed at the end of turn."],
 )
 elvish_mystic = CreatureCard(
     creature_type=ELF,
@@ -72,8 +72,7 @@ karametras_acolyte = CreatureCard(
     name="Karametra's Acolyte",
     power=1,
     toughness=4,
-    abilities=["Tap: Add an amount of Forest to your mana pool equal to your"
-    " devotion to green."],
+    abilities=["Tap: Add an amount of Forest to your mana pool equal to your devotion to green."],
 )
 llanowar_elves = CreatureCard(
     creature_type=ELF,
@@ -87,8 +86,8 @@ ryusei_the_falling_star = CreatureCard(
     name="Ryusei, the Falling Star",
     power=5,
     toughness=5,
-    abilities=["Flying\nWhen Ryusei, the Falling Star is put into a graveyard from"
-            " play, it deals 5 damage to each creature without flying. "],
+    abilities=["Flying"
+               "\nWhen Ryusei, the Falling Star is put into a graveyard from play, it deals 5 damage to each creature without flying. "],
 )
 savage_packmate = CreatureCard(
     creature_type=WEREWOLF,
@@ -104,9 +103,8 @@ slumbering_dragon = CreatureCard(
     name="Slumbering Dragon",
     power=3,
     toughness=3,
-    abilities=["Flying", "Slumbering Dragon can't attack or block unless"
-    "it has five or more +1/+1 counters on it. Whenever a creature attacks you"
-    "or a planeswalker you control, put a +1/+1 counter on Slumbering Dragon."],
+    abilities=["Flying"
+                "Slumbering Dragon can't attack or block unless it has five or more +1/+1 counters on it. Whenever a creature attacks you or a planeswalker you control, put a +1/+1 counter on Slumbering Dragon."],
 )
 snarling_wolf = CreatureCard(
     creature_type=WOLF,
@@ -122,36 +120,33 @@ stormbreath_dragon = CreatureCard(
     toughness=4,
     abilities=["Flying, haste, prtection from white"
             "\nUncoloredX5 and ChaosX2: Monstrosity 3."
-            "\n\nWhen Stormbreath Dragon becomes monstrous, it deals damage to"
-            "each opponent equal to the number of cards in that player's hand."],
+            "\n"
+            "\nWhen Stormbreath Dragon becomes monstrous, it deals damage to each opponent equal to the number of cards in that player's hand."],
 )
 storm_herald = CreatureCard(
     creature_type=HUMAN,
     name="Storm Herald",
     power=3,
     toughness=2,
-    abilities=["Haste\nWhen Storm Herald enter the battlefield, return any number"
-            "of Aura cards from your graveyard to the battlefield attached"
-            " to creatures you control. Exile all those Auras at the begining of"
-            " your next end step. If those Auras would leave the battlefield,"
-            " exile them instead of putting them anywhere else. "],
+    abilities=["Haste"
+               "\nWhen Storm Herald enters the battlefield, return any number of Aura cards from your graveyard to the battlefield attached to creatures you control. Exile all those Auras at the begining of your next end step. If those Auras would leave the"
+                " battlefield exile them instead of putting them anywhere else. "],
 )
 thunderbreak_regent = CreatureCard(
     creature_type=DRAGON,
     name="Thunderbreak Regent",
     power=4,
     toughness=4,
-    abilities=["Flying\nWhenever a Dragon you control becomes the target of a"
-    " spell or ability an opponent controls, Thunderbreak Regent"
-    " deals 3 damage to that player."],
+    abilities=["Flying"
+               "\nWhenever a Dragon you control becomes the target of a spell or ability an opponent controls, Thunderbreak Regent deals 3 damage to that player."],
 )
 tyrant_of_kher_ridges = CreatureCard(
     creature_type=DRAGON,
     name="Tyrant of Kher Ridges",
     power=4,
     toughness=5,
-    abilities=["Flying\nWhen Tyrant of Kher Ridges enters the battlefield, it"
-            " deals 4 damage to any target. "
+    abilities=["Flying"
+               "\nWhen Tyrant of Kher Ridges enters the battlefield, it deals 4 damage to any target. "
             "\nChaosX1: Tyrant of Kher Ridges gets +1/+0 until the end of turn"],
 )
 utvara_hellkite = CreatureCard(
@@ -159,16 +154,16 @@ utvara_hellkite = CreatureCard(
     name="Utvara Hellkite",
     power=6,
     toughness=6,
-    abilities=[ "Flying\nWhenever a Dragon you control attacks, put a 6/6 "
-    "red Dragon creature token with flying into the battlefield. "],
+    abilities=[ "Flying" 
+               "\nWhenever a Dragon you control attacks, put a 6/6 red Dragon creature token with flying into the battlefield. "],
 )
 utvara_corgi = CreatureCard(
     creature_type=WOLF,
     name="Utvara Corgi",
     power=2,
     toughness=2,
-    abilities=[ "Deathtouch, infect\nWhenever another creature attacks, put a 1/1 "
-    "Green wolf creature token with infect into the battlefield. "],
+    abilities=[ "Deathtouch, infect"
+               "\nWhenever another creature attacks, put a 1/1 Green wolf creature token with infect into the battlefield. "],
 )
 
 
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # main.py
 # This file is NOT the main entry point of the program, it is mostly used as a test file for logic.
 # For functioning programs go to: stats_returner.py, 
-from creature_card import CreatureCard
-from decks import test_deck, test_deck_dict
+from decks import (
+    test_creature1,
+    test_creature2,
+    test_creature3,
+    test_creature4,
+    test_creature5,
+    test_creature6,
+    test_creature7
+)
 from basic_land_card import ManaPool
-
 
 def cast_card(player_mana_pool, card):
     if hasattr(card, 'mana_cost') and isinstance(card.mana_cost, list):
@@ -30,4 +36,4 @@ def cast_card(player_mana_pool, card):
 
 player1_mana_pool = ManaPool()
 player1_mana_pool.add_mana("R", 1)
-cast_card(player1_mana_pool, test_deck_dict["test_creature1_dict"])
+cast_card(player1_mana_pool, test_creature1)
